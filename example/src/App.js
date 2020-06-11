7cbd23a0e6a24d7133792d7b32d3347cd17e6373
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-11 12:40:33 +0530

    Update App.js - Replace the newly generated App component with the one used in current example - Add DATA array

--- example/src/App.js
+++ example/src/App.js
@@ -3,8 +3,107 @@ import React from 'react'
 import { ExampleComponent } from 'react-audio-visualizer'
 import 'react-audio-visualizer/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+const DATA = [
+  {
+    model: {
+      path: './audio_one.mp3',
+      author: 'Galimatias & Joppe',
+      title: 'Mintaka'
+    },
+    options: { autoplay: false }
+  },
+  {
+    model: {
+      path: './audio_two.mp3',
+      author: 'NCT',
+      title: 'Rain Beyond The Sun'
+    },
+    options: { autoplay: true },
+    className: 'visualizer--custom-modifier'
+  }
+];
+
+const App = React.createClass({
+
+  getInitialState () {
+    return { item: DATA[0] };
+  },
+
+  onSelect (item) {
+    this.setState({ item });
+  },
+
+  onRenderStyle (context) {
+    // Render style decorator
+    // Write custom rendering style here
+  },
+
+  onRenderText (context) {
+    // Render text decorator
+    // Write custom rendering text here
+  },
+
+  onRenderTime (context) {
+    // Render time decorator
+    // Write custom rendering time here
+  },
+
+  onPlayStateChange (state) {
+
+    // Play state change notifier
+    switch (state.status) {
+
+      case 'BUFFERING':
+        break;
+
+      case 'PLAYING':
+        break;
+
+      case 'PAUSED':
+        break;
+
+      case 'ENDED':
+        break;
+    }
+  },
+
+  getLinks () {
+    return DATA.map((item, index) => {
+      return (
+        <li key={ index } onClick={ () => { this.onSelect(item); } }>
+          <div>{ item.model.author } - { item.model.title }</div>
+        </li>
+      );
+    });
+  },
+
+  getExtensions () {
+    return {
+      renderStyle: this.onRenderStyle,
+      renderText: this.onRenderText,
+      renderTime: this.onRenderTime
+    };
+  },
+
+  render () {
+    const links = this.getLinks();
+    const extensions = this.getExtensions();
+    const { item } = this.state;
+
+    return (
+      <div className="main">
+        <Visualizer
+          className={ item.className }
+          model={ item.model }
+          options={ item.options }
+          onChange={ this.onPlayStateChange }
+          width="800px"
+          height="400px" />
+
+        <ul>{ links }</ul>
+      </div>
+    );
+  }
+});
 
 export default App
